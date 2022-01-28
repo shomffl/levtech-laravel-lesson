c1fd0e64b3b67463c8f93f34b4f913696109bbd9
shomffl <showatanabe0226@gmail.com>
2022-01-28 13:27:11 +0900

    viewの作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="posts">
+            <div class="post">
+                <h2
+                class="title">title</h2>
+                <p class="body">This is a sample body.</p>
+            </div>
+        </div>
+    </body>
+</html>
